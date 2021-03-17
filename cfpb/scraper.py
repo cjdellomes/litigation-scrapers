3c6a8f6ebd7b7cacaf1178d3242fe333025c2b0a
cjdellomes <cjdellomes@gmail.com>
2021-03-16 17:25:08 -0700

    Add function for getting filtered litigation page
    The litigation page takes in filter parameters, such as a date range.
    The results are paginated, so we need to grab each page of results
    one at a time.

--- cfpb/scraper.py
+++ cfpb/scraper.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime
+from datetime import date
 from typing import List
 
 class Scraper:
@@ -7,12 +7,51 @@ class Scraper:
     enforcement_actions_url = cfpb_base_url + '/enforcement/actions'
     press_releases_url = cfpb_base_url + '/about-us/newsroom'
 
-    def __init__(self, categories:List[str], statuses:List[str], from_date:datetime, to_date:datetime):
+    def __init__(self, categories: List[str], statuses: List[str], from_date: date, to_date: date):
         self.categories = categories
         self.statuses = statuses
         self.from_date = from_date
         self.to_date = to_date
 
-    def get_enforcement_action_search_page():
-        response = requests.get(Scraper.enforcement_actions_url)
-        return response
+    def get_enforcement_action_search_page(self):
+        return requests.get(Scraper.enforcement_actions_url)
+
+    def get_enforcement_action_filtered_page(self, page_number: int):
+        url = self.enforcement_actions_url + '/?'
+
+        url = self._add_page_number_to_url(url, page_number)
+        url = self._add_categories_to_url(url, self.categories)
+        url = self._add_statuses_to_url(url, self.statuses)
+        url = self._add_from_date_to_url(url, self.from_date)
+        url = self._add_to_date_to_url(url, self.to_date)
+
+        return requests.get(url)
+
+    def _add_page_number_to_url(self, url: str, page_number: int):
+        if page_number <= 0:
+            return url
+        return url + 'page=' + str(page_number)
+    
+    def _add_categories_to_url(self, url: str, categories: List[str]):
+        if categories is None:
+            return url
+        for category in categories:
+            url += '&categories=' + category
+        return url
+
+    def _add_statuses_to_url(self, url: str, statuses: List[str]):
+        if statuses is None:
+            return url
+        for status in statuses:
+            url += '&statuses=' + status
+        return url
+
+    def _add_from_date_to_url(self, url: str, from_date: date):
+        if from_date is None:
+            return url
+        return url + '&from_date=' + from_date.isoformat()
+
+    def _add_to_date_to_url(self, url: str, to_date: date):
+        if to_date is None:
+            return url
+        return url + '&to_date=' + to_date.isoformat()
--- cfpb/scraper_test.py
+++ cfpb/scraper_test.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import date
 from scraper import Scraper
 
 class TestScraper(unittest.TestCase):
@@ -9,7 +10,23 @@ class TestScraper(unittest.TestCase):
 
         scraper = Scraper(None, None, None, None)
 
-        response = Scraper.get_enforcement_action_search_page()
+        response = scraper.get_enforcement_action_search_page()
+
+        self.assertEqual(response.status_code, expected_response_code)
+        self.assertEqual(response.url, expected_url)
+
+    def test_get_enforcement_action_filtered_page(self):
+        expected_response_code = 200
+        expected_url = 'https://www.consumerfinance.gov/enforcement/actions/?page=1&categories=administrative-proceeding&statuses=expired-terminated-dismissed&statuses=post-order-post-judgment&from_date=2013-01-14&to_date=2016-01-14'
+
+        categories = ['administrative-proceeding']
+        statuses = ['expired-terminated-dismissed', 'post-order-post-judgment']
+        from_date = date(2013,1,14)
+        to_date = date(2016,1,14)
+
+        scraper = Scraper(categories, statuses, from_date, to_date)
+
+        response = scraper.get_enforcement_action_filtered_page(1)
 
         self.assertEqual(response.status_code, expected_response_code)
         self.assertEqual(response.url, expected_url)
