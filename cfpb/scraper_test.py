5f1b9b8486c37ab555dc18026a35a8b152a24c6b
cjdellomes <cjdellomes@gmail.com>
2021-03-19 15:25:51 -0700

    Add enforcement action page partial filter test

--- cfpb/scraper_test.py
+++ cfpb/scraper_test.py
@@ -31,5 +31,21 @@ class TestScraper(unittest.TestCase):
         self.assertEqual(response.status_code, expected_response_code)
         self.assertEqual(response.url, expected_url)
 
+    def test_get_enforcement_action_filtered_page_some_filters_missing(self):
+        expected_response_code = 200
+        expected_url = 'https://www.consumerfinance.gov/enforcement/actions/?page=1&categories=administrative-proceeding&statuses=expired-terminated-dismissed'
+
+        categories = ['administrative-proceeding']
+        statuses = ['expired-terminated-dismissed']
+        from_date = None
+        to_date = None
+
+        scraper = Scraper(categories, statuses, from_date, to_date)
+
+        response = scraper.get_enforcement_action_filtered_page(1)
+
+        self.assertEqual(response.status_code, expected_response_code)
+        self.assertEqual(response.url, expected_url)
+
 if __name__ == '__main__':
     unittest.main()
