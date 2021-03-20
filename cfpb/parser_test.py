3f074673e9d50b8ee515edf868c6f9126f6046c4
cjdellomes <cjdellomes@gmail.com>
2021-03-20 16:07:44 -0700

    Rename parser tests to be more descriptive

--- cfpb/parser_test.py
+++ cfpb/parser_test.py
@@ -8,11 +8,11 @@ class TestParser(unittest.TestCase):
             expected_links = ['/enforcement/actions/foo/', '/enforcement/actions/bar/']
             self.assertCountEqual(Parser.get_enforcement_action_detail_links(test_page), expected_links)
 
-    def test_is_last_enforcement_action_filter_page_should_return_false(self):
+    def test_is_last_enforcement_action_filter_page_enabled_button_should_return_false(self):
         test_page = '<p>blah</p><div><a class="a-btn m-pagination_btn-next"></a></div>'
         self.assertFalse(Parser.is_last_enforcement_action_filter_page(test_page))
 
-    def test_is_last_enforcement_action_filter_page_should_return_true(self):
+    def test_is_last_enforcement_action_filter_page_disabled_button_should_return_true(self):
         test_page = '<div><a class="a-btn a-btn__disabled m-pagination_btn-next"></a></div>'
         self.assertTrue(Parser.is_last_enforcement_action_filter_page(test_page))
 
