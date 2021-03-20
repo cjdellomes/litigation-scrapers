7032e5158632e823abcc26033e1c8272e5746289
cjdellomes <cjdellomes@gmail.com>
2021-03-20 14:06:26 -0700

    Make the url builder functions idempotent
    Instead of concatenating straight to the url on each parameter function
    call, adjust the functions so that each call sets the parameter. Move
    the string concatenation to the build function.
    
    This makes it so the order in which the parameters are set doesn't
    matter. This also enseures no accidental adding of a parameteter more
    than needed (e.g. setting 2 different from dates).

--- cfpb/urlbuilder.py
+++ cfpb/urlbuilder.py
@@ -4,45 +4,59 @@ from typing import List
 class UrlBuilder:
 
     def __init__(self, base_url='', subdirectory=''):
-        self.url = base_url + subdirectory
+        self.base_url = base_url
+        self.sub_directory = subdirectory
 
     def build(self):
-        return self.url
+        return self.base_url + self.sub_directory
 
 class EnforcementActionFilterUrlBuilder(UrlBuilder):
 
     def __init__(self, base_url='', page_number=0, categories=[], statuses=[], from_date=None, to_date=None):
         super().__init__(base_url=base_url)
-        self.url += '/?'
         self.page_number(page_number)
         self.categories(categories)
         self.statuses(statuses)
         self.from_date(from_date)
         self.to_date(to_date)
 
+    def build(self):
+        return (self.base_url
+                + '/?'
+                + self.page_number
+                + self.categories
+                + self.statuses
+                + self.from_date
+                + self.to_date)
+
     def page_number(self, page_number: int):
+        self.page_number = ''
         if page_number <= 0:
             return
-        self.url += '&page=' + str(page_number)
+        self.page_number = '&page=' + str(page_number)
         
     def categories(self, categories: List[str]):
+        self.categories = ''
         if categories is None:
             return
         for category in categories:
-            self.url += '&categories=' + category
+            self.categories += '&categories=' + category
 
     def statuses(self, statuses: List[str]):
+        self.statuses = ''
         if statuses is None:
             return
         for status in statuses:
-            self.url += '&statuses=' + status
+            self.statuses += '&statuses=' + status
 
     def from_date(self, from_date: date):
+        self.from_date = ''
         if from_date is None:
             return
-        self.url += '&from_date=' + from_date.isoformat()
+        self.from_date = '&from_date=' + from_date.isoformat()
 
     def to_date(self, to_date: date):
+        self.to_date = ''
         if to_date is None:
             return
-        self.url += '&to_date=' + to_date.isoformat()
+        self.to_date = '&to_date=' + to_date.isoformat()
